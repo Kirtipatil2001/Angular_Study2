556609e54a1a90eefe6451a20d81b9d6c38d87c8
Kirti Ravindra Patil <79988064+Kirtipatil2001@users.noreply.github.com>
2025-02-08 12:39:18 +0530

    Sending HTTP Requests and Handling Response
    How to connect angular app to backend
    Angular HTTP client
    Sending a GET request to fetch data 
    configuring Http requests
    Transforming an using Response Data
    Loading Fallback
    Handling HTTP errors
    Sending Data to a backend
    More data fetching and some code Duplication 
    Outsourcing HTTP Request logic to a services 
    Managing HTTP-loaded data via a services
    Improved Optimistic Updating 
    Implementing APP-WIDE- ERROR management
    Sending DELETE request
    HTTP Inceptors

---
+++ src/app/places/user-places/user-places.component.ts
@@ -0,0 +1,51 @@
+import { Component, DestroyRef, inject, signal } from '@angular/core';
+
+import { PlacesContainerComponent } from '../places-container/places-container.component';
+import { PlacesComponent } from '../places.component';
+import { Place } from '../place.model';
+import { catchError, map, throwError } from 'rxjs';
+import { PlacesService } from '../places.service';
+
+@Component({
+  selector: 'app-user-places',
+  standalone: true,
+  templateUrl: './user-places.component.html',
+  styleUrl: './user-places.component.css',
+  imports: [PlacesContainerComponent, PlacesComponent],
+})
+export class UserPlacesComponent {
+onSelectPlace($event: Place) {
+throw new Error('Method not implemented.');
+}
+  isFetching  = signal(false);
+  error = signal('');
+  private placesService = inject(PlacesService);
+  private destroyRef = inject(DestroyRef);
+  places = this.placesService.loadedUserPlaces;
+
+  ngOnInit(){
+    this.isFetching.set(true);
+    const subscription = this.placesService.loadUserPlaces()
+   .subscribe({
+      error: (error: Error) => {
+
+        this.error.set(error.message);
+      },
+      complete: () => {
+        this.isFetching.set(false);
+      }
+    });
+
+    this.destroyRef.onDestroy(() => {
+      subscription.unsubscribe();
+    });
+  }
+
+  onRemovePlaces(place: Place){
+   const subscription = this.placesService.removeUserPlace(place).subscribe();
+
+    this.destroyRef.onDestroy(() => {
+    subscription.unsubscribe();
+  });
+  }
+}
